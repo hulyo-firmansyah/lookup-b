20f595846db2963b18e37554b70df4ff01415104
= <=>
2022-12-05 13:49:18 +0700

    CRUD category

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\UserAuthController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,9 @@ use App\Http\Controllers\Auth\UserAuthController;
 Route::post('/login', [UserAuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
-    Route::middleware('auth:api')->post('/logout', [UserAuthController::class, 'logout']);
+    Route::post('/logout', [UserAuthController::class, 'logout']);
+
+    Route::apiResource('categories', CategoryController::class);
 });
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
