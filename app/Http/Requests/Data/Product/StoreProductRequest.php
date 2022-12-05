62ca06f95bd78b5737ceb8c852e00e475d4b596e
= <=>
2022-12-05 16:33:51 +0700

    productRequest

--- app/Http/Requests/Data/Product/StoreProductRequest.php
+++ app/Http/Requests/Data/Product/StoreProductRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Data\Product;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateProductRequest extends FormRequest
+class StoreProductRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
