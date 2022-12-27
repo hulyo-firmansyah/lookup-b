35430e204106ebb94b679bda19aa821b1b332f45
Hul <axusxzbitcomp@gmail.com>
2022-12-27 15:34:05 +0700

    update welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -400,7 +400,12 @@
 
 <body class="antialiased">
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0" style="color:white">
-        <h1>Welcome | RPK APP</h1>
+        <div>
+            <h1>Welcome | RPK APP</h1>
+            <div>
+                <h2>Superapp</h2>
+            </div>
+        </div>
     </div>
 </body>
 
