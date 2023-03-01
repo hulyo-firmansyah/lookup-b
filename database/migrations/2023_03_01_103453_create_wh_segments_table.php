0698e2d0fc17a41dc5b3365cb7b73a3125bd727a
Hul <axusxzbitcomp@gmail.com>
2023-03-01 17:40:05 +0700

    wh_segments, prd_units

---
+++ database/migrations/2023_03_01_103453_create_wh_segments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWhSegmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wh_segments', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 20)->nullable();
+            $table->text('image_path')->nullable();
+            $table->string('name', 100);
+            $table->text('details')->nullable();
+            $table->foreignId('wh_id')->constrained('warehouses')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('type_id')->constrained('wh_segtypes')->onDelete('restrict')->onUpdate('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wh_segments');
+    }
+}
