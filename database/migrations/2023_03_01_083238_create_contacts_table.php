45ce13818cbd47b6f88ea08b0123e105dce6ee71
Hul <axusxzbitcomp@gmail.com>
2023-03-01 15:35:22 +0700

    contacts

---
+++ database/migrations/2023_03_01_083238_create_contacts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('display_name', 100);
+            $table->foreignId('def_payment_terms_id')->nullable()->constrained('payment_terms')->onDelete('restrict')->onUpdate('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
